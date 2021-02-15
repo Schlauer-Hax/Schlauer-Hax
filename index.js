649c1065fd70ccf8922801cf2f0cc300cde1efc4
Schlauer-Hax <arzberger52@gmail.com>
2021-02-15 18:21:38 +0100

    progressbar

--- index.js
+++ index.js
@@ -9,29 +9,26 @@ function generateMonthBar() {
     const now = new Date()
     const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
     const endOfMonth = new Date(now.getFullYear(), now.getMonth()+1, 0);
-    const percentOfMonth = (now - startOfMonth) / (endOfMonth - startOfMonth)
+    const percent = (now - startOfMonth) / (endOfMonth - startOfMonth)
 
-    const passedProgressBarIndex = parseInt(percentOfMonth * progressBarCapacity)
-    const progressBar = Array(progressBarCapacity)
-        .fill('▁')
-        .map((value, index) => index < passedProgressBarIndex ? '█' : value)
-        .join('')
-    return {bar: progressBar, percent: percentOfMonth}
+    return {bar: generateProgressbar(percent), percent: percent}
 }
 
 function generateYearBar() {
     const thisYear = new Date().getFullYear()
     const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
     const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
-    const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
+    const percent = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear)
 
-    const progressBarCapacity = 30
-    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity)
-    const progressBar = Array(progressBarCapacity)
-        .fill('▁')
+    return {bar: generateProgressbar(percent), percent: percent}
+}
+
+function generateProgressbar(percent) {
+    const passedProgressBarIndex = parseInt(percent * progressBarCapacity)
+    return Array(progressBarCapacity)
+        .fill('░')
         .map((value, index) => index < passedProgressBarIndex ? '█' : value)
         .join('')
-    return {bar: progressBar, percent: progressOfThisYear}
 }
 
 

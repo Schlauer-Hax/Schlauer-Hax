3f79aa9277ab714af28028bb36a2257c918155b8
Schlauer-Hax <hax@bbn.family>
2022-01-30 00:00:55 +0100

    Fixed Month Progress, thanks to @andi-makes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function generateMonthBar() {
     const now = new Date()
     const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
     const endOfMonth = new Date(now.getFullYear(), now.getMonth()+1, 0);
+    endOfMonth.setHours(23, 59, 59, 999)
     const percent = (now - startOfMonth) / (endOfMonth - startOfMonth)
 
     return {bar: generateProgressbar(percent), percent: percent}
